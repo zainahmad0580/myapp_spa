e02b56e1b3bbff8fd0b25a87c32856c6e76a74f7
zainahmad0580 <zainahmad0580@gmail.com>
2023-05-02 09:22:55 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
